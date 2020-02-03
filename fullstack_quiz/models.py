5f7d4f927e4f17a980b13181d0a3bc5fd038f58b
Jessica Biondi <jbiondi.cs@gmail.com>
2020-02-03 11:59:58 -0800

    Models and their migration

--- fullstack_quiz/models.py
+++ fullstack_quiz/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Quiz(models.Model):
+    quiz_id = models.AutoField(primary_key=True)
+    quiz_topic = models.CharField(max_length=50)
+
+
+class Question(models.Model):
+    question_id = models.AutoField(primary_key=True)
+    quiz_id = models.ForeignKey(Quiz, null=True, on_delete=models.SET_NULL)
+    question_text = models.TextField()
+    incorrect_answer1 = models.CharField(max_length=50)
+    incorrect_answer2 = models.CharField(max_length=50)
+    incorrect_answer3 = models.CharField(max_length=50)
+    correct_answer = models.CharField(max_length=50)
+
+
+class QuizUser(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    display_name = models.CharField(max_length=25)
+    avatar = models.URLField()
+
+
+class Score(models.Model):
+    score_id = models.AutoField(primary_key=True)
+    quiz_id = models.ForeignKey(Quiz, null=True, on_delete=models.SET_NULL)
+    user_id = models.ForeignKey(QuizUser, null=True, on_delete=models.SET_NULL)
+    score_timestamp = models.DateTimeField(default=timezone.now)
+    score_value = models.CharField(max_length=20)
+
+
+
