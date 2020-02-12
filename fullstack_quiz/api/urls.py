ca85d81302cdfbf855cae67171e4da5fc23e4fa5
Jessica Biondi <jbiondi.cs@gmail.com>
2020-02-12 10:49:08 -0800

    Added api urls

--- fullstack_quiz/api/urls.py
+++ fullstack_quiz/api/urls.py
@@ -6,5 +6,5 @@ from fullstack_quiz.api.views import api_detail_quiz_view
 app_name = 'fullstack_quiz'
 
 urlpatterns = [
-    path('<slug>', api_detail_quiz_view, name='detail'),
+    path('<slug>/', api_detail_quiz_view, name='detail'),
 ]
