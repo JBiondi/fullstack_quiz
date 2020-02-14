37f77b128d3c31fdcc4d029836f09f6cc428324d
Jessica Biondi <jbiondi.cs@gmail.com>
2020-02-14 13:27:45 -0800

    Formatting

--- fullstack_quiz_project/settings.py
+++ fullstack_quiz_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'fullstack_quiz.apps.FullstackQuizConfig',
     'rest_framework',
 ]
