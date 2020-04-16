5f1be5e580dceeb22839a3467e6b7a52365bb511
Jessica Biondi <jbiondi.cs@gmail.com>
2020-04-16 13:27:29 -0700

    Hide quiz elements until a selection is made

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,21 @@
+// Selection screen elements
+const chooseHeader = document.querySelector('.choose-h1');
 const selectionVideoGameQuotes = document.querySelector('.selection-vg-quotes');
 const selectionProgramming = document.querySelector('.selection-programming');
+
+// Quiz elements
 const promptAnswerContainer = document.querySelector('.prompt-answer-container');
+const quizTitle = document.querySelector('.quiz-title');
+const nextButton = document.querySelector('.next-button');
+const choicesContainer = document.querySelector('.choices-container');
+const choice0 = document.querySelector('.choice0');
+const choice1 = document.querySelector('.choice1');
+const choice2 = document.querySelector('.choice2');
+const choice3 = document.querySelector('.choice3');
+
+//
+const correctNotification = document.querySelector('.correct-notification');
+const incorrectNotification = document.querySelector('.incorrect-notification');
 
 
 // Add event listeners
@@ -26,11 +41,24 @@ function getCookie(name) {
 const csrftoken = getCookie('csrftoken');
 
 
+function showQuizElements() {
+    promptAnswerContainer.style.display = 'block';
+    nextButton.style.display = 'block';
+    choicesContainer.style.display = 'block';
+
+}
 function selectedVGQuotes() {
+
+    showQuizElements();
 
     // Remove event listeners
     selectionVideoGameQuotes.removeEventListener('click', selectedVGQuotes);
     selectionProgramming.removeEventListener('click', selectedProgramming);
+
+    // Hide choice related elements
+    selectionVideoGameQuotes.style.display = 'none';
+    selectionProgramming.style.display = 'none';
+    chooseHeader.style.display = 'none';
     
     // Tell the backend we've chosen this quiz
     const selectedQuizID = 1;
@@ -53,6 +81,10 @@ function selectedVGQuotes() {
             data.forEach(prompt => console.log(prompt))
             promptAnswerContainer.innerHTML = `${data[0].prompt_text}`
         })
+
+    // Show title
+    quizTitle.innerHTML = 'Video Game Quotes'
+
 }
 
 
