033aff141a15e75c96cdef9b1cea611df3f7241a
Jessica Biondi <jbiondi.cs@gmail.com>
2020-04-22 08:32:02 -0700

    working view to render model form for display name

--- api/views.py
+++ api/views.py
@@ -1,15 +1,12 @@
 import json
 
-from django.http import HttpRequest
 from django.http import HttpResponse
-from django.shortcuts import render
 
-from fullstack_quiz.forms import DisplayNameForm
 from fullstack_quiz.models import Prompt
 from .serializers import PromptSerializer
 
 
-def quiz_selection_handler_view(request: HttpRequest, selected_quiz_id=None):
+def quiz_selection_handler_view(request, selected_quiz_id=None):
 
     prompts_queryset = Prompt.objects.filter(quiz_id=selected_quiz_id).order_by('prompt_id')
 
@@ -21,16 +18,4 @@ def quiz_selection_handler_view(request: HttpRequest, selected_quiz_id=None):
 
     return HttpResponse(json_prompts, content_type='application/json')
 
-
-def display_name_form_view(request):
-    if request.method == 'POST':
-        form = DisplayNameForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-
-    form = DisplayNameForm()
-
-    context = {'form': form}
-    return render(request, 'frontend/index.html', context)
 
--- frontend/views.py
+++ frontend/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from fullstack_quiz.forms import DisplayNameForm
 
 
 def frontend_home_view(request):
     print('pomegranate')
-    return render(request, 'frontend/index.html')
+    if request.method == 'POST':
+        form = DisplayNameForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+        return
+
+    form = DisplayNameForm()
+
+    context = {'form': form}
+    return render(request, 'frontend/index.html', context)
+
 
