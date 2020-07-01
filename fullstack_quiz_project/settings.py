105f83fac16b4140a89af04a1927e1b2586e7422
Jessica Biondi <jbiondi.cs@gmail.com>
2020-07-01 14:29:42 -0700

    Added Heroku site to allowed hosts

--- fullstack_quiz_project/settings.py
+++ fullstack_quiz_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG_VALUE')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://git.heroku.com/fullstack-quiz-website.git']
 
 # Application definition
 
