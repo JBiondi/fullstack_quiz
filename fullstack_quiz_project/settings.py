0a0a35c08bc634ac63f72d04ee290f5bdd11d872
Jessica Biondi <jbiondi.cs@gmail.com>
2020-07-14 08:42:36 -0700

    Updating allowed hosts

--- fullstack_quiz_project/settings.py
+++ fullstack_quiz_project/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG_VALUE')
 
-ALLOWED_HOSTS = ['fullstack-quiz-website.herokuapp.com']
+ALLOWED_HOSTS = ['fullstack-quiz-website.herokuapp.com',
+                 'www.cozyquiz.com',
+                 'cozyquiz.com']
 
 # Application definition
 
