cdb2a8ddfa37d77e4f0cded7fbd9989b900ea482
Jessica Biondi <jbiondi.cs@gmail.com>
2020-07-17 11:53:43 -0700

    Refactoring receive user score view

--- api/views.py
+++ api/views.py
@@ -26,12 +26,13 @@ def quiz_selection_handler_view(request, selected_quiz_id=None):
 def receive_user_score_view(request, user_correct_score=None, quiz_id=None):
     if request.method == 'POST':
 
-        current_quiz = quiz_id
-        current_attempt = HighScore(user_correct_score=user_correct_score, associated_quiz_id=current_quiz)
+        current_attempt = HighScore(user_correct_score=user_correct_score, associated_quiz_id=quiz_id)
         current_attempt.save()
 
         current_attempt_id = current_attempt.high_score_id
         request.session['current attempt ID'] = current_attempt_id
+        request.session.modified = True
+
         json_current_attempt_id = json.dumps(current_attempt_id)
 
         return HttpResponse(json_current_attempt_id, content_type='application/json')
